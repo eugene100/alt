945bf21d64454c965e9573e98b132d3e6898e538
Yevhen Stolietnii <eugene100@eugene100.org.ua>
2023-10-29 14:25:28 +0200

    fixes

--- terraform/dev/eks/cluster1/main.tf
+++ terraform/dev/eks/cluster1/main.tf
@@ -9,17 +9,17 @@ terraform {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket                  = "wl-dev-tfstate"
-    key                     = "dev/main.tfstate"
-    region                  = "us-east-1"
+    bucket = "wl-dev-tfstate"
+    key    = "dev/main.tfstate"
+    region = "us-east-1"
   }
 }
 
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket                  = "wl-dev-tfstate"
-    key                     = "dev/eks/main.tfstate"
-    region                  = "us-east-1"
+    bucket = "wl-dev-tfstate"
+    key    = "dev/eks/main.tfstate"
+    region = "us-east-1"
   }
 }
